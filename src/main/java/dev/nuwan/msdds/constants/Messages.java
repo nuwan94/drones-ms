e215eeccbae230cc136895e4430c51cd0f63ea74
Restyled.io <commits@restyled.io>
2022-10-09 21:42:20 +0000

    Restyled by clang-format

--- src/main/java/dev/nuwan/msdds/constants/Messages.java
+++ src/main/java/dev/nuwan/msdds/constants/Messages.java
@@ -7,13 +7,17 @@ public final class Messages {
   public static final String DRONE_INVALID_MODEL = "Invalid drone model";
   public static final String DRONE_INVALID_STATE = "Invalid drone state";
   public static final String DRONE_NOT_EXISTS = "Drone doesn't exists";
-  public static final String DRONE_WEIGHT_LIMIT_EXCEED = "Can't load this medication to drone'";
-  public static final String DRONE_LOADED_SUCCESSFULLY = "Medication loaded to drone successfully";
+  public static final String DRONE_WEIGHT_LIMIT_EXCEED =
+      "Can't load this medication to drone'";
+  public static final String DRONE_LOADED_SUCCESSFULLY =
+      "Medication loaded to drone successfully";
   public static final String DRONE_FOUND = "Drone found";
   public static final String MEDICATION_SAVED = "Medication saved successfully";
-  public static final String MEDICATION_DELETED = "Medication deleted successfully";
-  public static final String MEDICATION_DELETE_FAILURE = "Medication delete failed";
+  public static final String MEDICATION_DELETED =
+      "Medication deleted successfully";
+  public static final String MEDICATION_DELETE_FAILURE =
+      "Medication delete failed";
   public static final String MEDICATION_EXISTS = "Medication exists";
-  public static final String MEDICATION_NOT_EXISTS = "Medication doesn't exists";
-
+  public static final String MEDICATION_NOT_EXISTS =
+      "Medication doesn't exists";
 }
--- src/main/java/dev/nuwan/msdds/constants/StatusCodes.java
+++ src/main/java/dev/nuwan/msdds/constants/StatusCodes.java
@@ -3,8 +3,8 @@ package dev.nuwan.msdds.constants;
 public final class StatusCodes {
 
   public static final Integer SUCCESS = 0;
-  public static final  Integer FAILURE = -1;
-  public static final  Integer DUPLICATE = 101;
-  public static final  Integer INVALID = 102;
+  public static final Integer FAILURE = -1;
+  public static final Integer DUPLICATE = 101;
+  public static final Integer INVALID = 102;
   public static final Integer NOT_FOUND = 404;
 }
